6d3c040df63239cb66cbe71374131dc3b528174f
Christian Glissov <cdglissov@gmail.com>
2023-06-19 20:01:35 +0200

    added webpack envs and min

--- webpack.common.js
+++ webpack.common.js
@@ -1,36 +1,13 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-    mode: 'development',
     entry: {
         main: path.resolve(__dirname, 'src/js/index.js'),
-        toc: path.resolve(__dirname, 'src/js/toc.js')
+        toc: path.resolve(__dirname, 'src/js/index_toc.js')
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        assetModuleFilename: 'assets/[name][ext]',
-        clean: true
-    },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist')
-        },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true
-    },
-    devtool: 'source-map',
     module: {
         rules: [
-            {
-                test:/\.scss$/,
-                use: [
-                    'style-loader', 'css-loader', 'sass-loader'
-                ]
-            },
             {
                 test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
                 type: 'asset/resource',
@@ -51,17 +28,18 @@ module.exports = {
         ]
     },
     plugins: [
+        // autogenerate html files to adapt to bundles
         new HtmlWebpackPlugin({
-            title: 'Webpack app',
+            title: 'home',
             filename: 'index.html',
             template: 'src/index.html',
             chunks: ['main']
         }),
         new HtmlWebpackPlugin({
-            title: 'Webpack app',
+            title: 'blog',
             filename: 'tableofcontents.html',
             template: 'src/tableofcontents.html',
             chunks: ['toc']
-        })
-    ]
-}
+        }),
+        ]
+    }
